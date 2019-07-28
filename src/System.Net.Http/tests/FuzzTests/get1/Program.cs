8ec3a6d10fb381b54529ba8db921caeea13ac04d
Cory Nelson <phrosty@gmail.com>
2019-07-28 16:30:59 -0700

    Wait to send data until HttpClient sends headers, this way we know a stream is created and ready to receive response data. Disable SSL due to ALPN not working on MSRD server.

--- src/System.Net.Http/tests/FuzzTests/get1/Program.cs
+++ src/System.Net.Http/tests/FuzzTests/get1/Program.cs
@@ -9,6 +9,7 @@ using System.Net.Sockets;
 using System.Net.Test.Common;
 using System.Runtime.CompilerServices;
 using System.Security.Policy;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,6 +19,9 @@ namespace get1
     {
         static async Task Main(string[] args)
         {
+            // 2008 R2 on MSRD does not support ALPN.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
             byte[] payload = File.ReadAllBytes(args[0]);
 
             using Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -26,58 +30,138 @@ namespace get1
             listenSocket.Listen(1);
 
             var localEndPoint = (IPEndPoint)listenSocket.LocalEndPoint;
-            string uri = $"https://{localEndPoint.Address}:{localEndPoint.Port}/";
+            string uri = $"http://{localEndPoint.Address}:{localEndPoint.Port}/";
+            Console.WriteLine($"Testing via {uri}");
 
             var handler = new SocketsHttpHandler()
             {
                 SslOptions = new System.Net.Security.SslClientAuthenticationOptions
                 {
                     RemoteCertificateValidationCallback = delegate { return true; }
-                }
+                },
+                UseProxy = false
             };
 
             using HttpClient client = new HttpClient(handler);
             client.DefaultRequestVersion = new Version(2, 0);
+            client.Timeout = Timeout.InfiniteTimeSpan;
+            client.DefaultRequestHeaders.Add("x-foo-ready", "1");
 
             Task clientTask = client.GetStringAsync(uri);
 
             using Socket acceptSock = await listenSocket.AcceptAsync();
             using Stream acceptStream = new NetworkStream(acceptSock, true);
-            using SslStream sslStream = new SslStream(acceptStream, false, delegate { return true; });
+            //using SslStream sslStream = new SslStream(acceptStream, false, delegate { return true; });
 
-            using (var cert = System.Net.Test.Common.Configuration.Certificates.GetServerCertificate())
+            //using (var cert = System.Net.Test.Common.Configuration.Certificates.GetServerCertificate())
+            //{
+            //    SslServerAuthenticationOptions options = new SslServerAuthenticationOptions();
+
+            //    options.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+
+            //    var protocols = new List<SslApplicationProtocol>();
+            //    protocols.Add(SslApplicationProtocol.Http2);
+            //    protocols.Add(SslApplicationProtocol.Http11);
+            //    options.ApplicationProtocols = protocols;
+
+            //    options.ServerCertificate = cert;
+
+            //    options.ClientCertificateRequired = false;
+
+            //    await sslStream.AuthenticateAsServerAsync(options);
+            //}
+
+            await DoReadUntilHeaders();
+            _ = DoRead();
+            _ = DoWrite();
+
+            try
             {
-                SslServerAuthenticationOptions options = new SslServerAuthenticationOptions();
-
-                options.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
-
-                var protocols = new List<SslApplicationProtocol>();
-                protocols.Add(SslApplicationProtocol.Http2);
-                protocols.Add(SslApplicationProtocol.Http11);
-                options.ApplicationProtocols = protocols;
-
-                options.ServerCertificate = cert;
-
-                options.ClientCertificateRequired = false;
-
-                await sslStream.AuthenticateAsServerAsync(options);
+                Console.WriteLine("Waiting for HttpClient...");
+                await clientTask;
+            }
+            finally
+            {
+                Console.WriteLine("Done with HttpClient.");
             }
 
-            // read once so we don't send stream data before HTTPClient has initialized its own streams.
-            byte[] readbuf = new byte[1024];
-            await sslStream.ReadAsync(readbuf.AsMemory());
+            async Task DoReadUntilHeaders()
+            {
+                Console.WriteLine("Waiting for headers...");
+                try
+                {
+                    byte[] expected = Encoding.ASCII.GetBytes("x-foo-ready");
+                    byte[] readbuf = new byte[1024];
+                    int readpos = 0;
 
-            _ = DoRead();
-            _ = sslStream.WriteAsync(payload.AsMemory());
+                    while (true)
+                    {
+                        if (readpos == readbuf.Length)
+                        {
+                            throw new Exception("Never received headers.");
+                        }
 
-            await clientTask;
+                        int len = await acceptStream.ReadAsync(readbuf.AsMemory(readpos));
+
+                        if (len == 0)
+                        {
+                            throw new Exception("Never received headers.");
+                        }
+
+                        readpos += len;
+
+                        for (int i = 0; i < readpos; ++i)
+                        {
+                            if (readbuf.AsSpan(i, Math.Min(readpos - i, expected.Length)).SequenceEqual(expected))
+                            {
+                                Console.WriteLine("Found headers.");
+                                return;
+                            }
+                        }
+
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine("Done waiting for headers.");
+                }
+            }
 
             async Task DoRead()
             {
-                int len;
+                Console.WriteLine("Reading...");
+                try
+                {
+                    byte[] readbuf = new byte[1024];
+                    int len;
 
-                do len = await sslStream.ReadAsync(readbuf.AsMemory());
-                while (len != 0);
+                    do
+                        len = await acceptStream.ReadAsync(readbuf.AsMemory());
+                    while (len != 0);
+                }
+                finally
+                {
+                    Console.WriteLine("Done Reading.");
+                }
+            }
+
+            async Task DoWrite()
+            {
+                Console.WriteLine("Writing...");
+                try
+                {
+                    // Wait for HttpClient to initiate its stream ID, or writing payload will fail.
+                    await Task.Delay(500);
+
+                    await acceptStream.WriteAsync(payload.AsMemory());
+                    await acceptStream.FlushAsync();
+                    //await sslStream.ShutdownAsync();
+                    acceptSock.Shutdown(SocketShutdown.Send);
+                }
+                finally
+                {
+                    Console.WriteLine("Done Writing.");
+                }
             }
         }
     }
