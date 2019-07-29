36a62137b87a1e76b0de5ea63621db18a3f81405
Cory Nelson <phrosty@gmail.com>
2019-07-29 14:54:14 -0700

    Augment app to group exceptions by their stack trace.

--- src/System.Net.Http/tests/FuzzTests/hpack1/Program.cs
+++ src/System.Net.Http/tests/FuzzTests/hpack1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http.HPack;
 using System.Text;
 
@@ -9,7 +12,45 @@ namespace hpack1
     {
         static void Main(string[] args)
         {
-            byte[] data = File.ReadAllBytes(args[0]);
+            string path = args[0];
+
+            var exceptions =
+                from filePath in Directory.EnumerateFiles(path, "*.bin", SearchOption.TopDirectoryOnly)
+                from exception in RunOne(filePath)
+                group (exception, filePath) by exception.StackTrace into grp
+                select grp
+                    .OrderByDescending(ex_fp => new FileInfo(ex_fp.filePath).Length)
+                    .First();
+
+            foreach (var (exception, filePath) in exceptions)
+            {
+                Console.WriteLine(filePath);
+                Console.WriteLine("===============");
+                Console.WriteLine(exception.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        static IEnumerable<Exception> RunOne(string filePath)
+        {
+            try
+            {
+                RunOneImpl(filePath);
+                return Enumerable.Empty<Exception>();
+            }
+            catch (AggregateException ex)
+            {
+                return ex.Flatten().InnerExceptions;
+            }
+            catch (Exception ex)
+            {
+                return new[] { ex };
+            }
+        }
+
+        static void RunOneImpl(string filePath)
+        {
+            byte[] data = File.ReadAllBytes(filePath);
 
             HPackDecoder decoder = new HPackDecoder();
 
@@ -17,7 +58,7 @@ namespace hpack1
             {
                 decoder.Decode(data, true, (state, name, value) =>
                 {
-                    Console.WriteLine(Encoding.ASCII.GetString(name) + ": " + Encoding.ASCII.GetString(value));
+                    //Console.WriteLine(Encoding.ASCII.GetString(name) + ": " + Encoding.ASCII.GetString(value));
                 }, null);
 
                 decoder.CompleteDecode();
